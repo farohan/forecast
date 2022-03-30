4e909f9fa7f9d665e35d4c4fd3660ebc852f2a64
farohan <68710283+farohan@users.noreply.github.com>
2022-03-29 17:55:28 -0700

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,1 +1,17 @@
+let btn = document.getElementById('button');
 
+const apiKey = '515f3fa5d2503494452e23251073518b';
+const url = `api.openweathermap.org/data/2.5/weather?london,uk&APPID=${apiKey}`;
+
+console.log(url);
+
+function fetchWeather() {
+    fetch(url) 
+        .then((response) => {
+            return response.json();
+        }).then((data) => {
+            console.log(data);
+    });
+}
+
+btn.addEventListener('click', fetchWeather);

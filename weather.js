5f9c2986053bf01619fa97dd19d9c8f97236674d
farohan <68710283+farohan@users.noreply.github.com>
2022-04-25 21:27:08 -0700

    Update weather.js

--- weather.js
+++ weather.js
@@ -9,8 +9,9 @@ function getCity() {
     fetchWeather();
 }
 
-//Selecting the icon
+//Selecting the application box & the icon
 const icon = document.getElementById('w-icon');
+const application = document.querySelector('.app');
 
 function fetchWeather() {
     const apiKey = '515f3fa5d2503494452e23251073518b';
@@ -25,12 +26,29 @@ function fetchWeather() {
 
             console.log(data);
 
-            const fetchedData = [data.main.feels_like, data.name, data.sys.country, data.weather[0].description, data.main.humidity, data.wind.speed, data.clouds.all, data.coord.lat, data.coord.lon];
-            for (let i = 0; i < fetchedData.length; i++) {
-                console.log(fetchedData[i]);
-            }
+            const fetchedData = [data.main.feels_like, data.name, data.sys.country, data.weather[0].description, data.main.humidity, data.wind.speed, data.clouds.all, data.visibility, data.coord.lat, data.coord.lon];
 
-            const allParagraphs = []; //list of all the paragraphs
+            const allParagraphs = [
+                document.querySelector('#city-temp'),
+                document.querySelector('#city-country'),
+                document.querySelector('#desc'),
+                document.querySelector('#humid'),
+                document.querySelector('#w-speed'),
+                document.querySelector('#cloud-percent'),
+                document.querySelector('#visible'),
+                document.querySelector('#lat'),
+                document.querySelector('#lon')
+            ];
+
+            allParagraphs[0].innerHTML = `${Math.floor(1.8 * (fetchedData[0] - 273) + 32)}°F`;
+            allParagraphs[1].innerHTML = `${fetchedData[1]}, ${fetchedData[2]}`;
+            allParagraphs[2].innerHTML = `Description: ${fetchedData[3]}.`;
+            allParagraphs[3].innerHTML = `Humidity: ${fetchedData[4]}%`;
+            allParagraphs[4].innerHTML = `Wind Speed: ${fetchedData[5]} MPH`;
+            allParagraphs[5].innerHTML = `Cloudiness: ${fetchedData[6]}%`;
+            allParagraphs[6].innerHTML = `Visibility: About ${Math.floor(fetchedData[7] / 1609)} mile(s)`;
+            allParagraphs[7].innerHTML = `Latitude: ${fetchedData[8]}`;
+            allParagraphs[8].innerHTML = `Longitude: ${fetchedData[9]}`;
     });
 }
 
